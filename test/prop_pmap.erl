3c878c0fcb4ffc852eaaed2f0d1a8d5bf66c8e73
Sergey Prokhorov <sergey.prokhorov@klarna.com>
2024-09-16 22:11:39 +0200

    Make sure several pmap iterators can be chained and not affect each other

--- test/prop_pmap.erl
+++ test/prop_pmap.erl
@@ -5,7 +5,8 @@
 -export([
     prop_order_is_preserved/0,
     prop_vs_lists/0,
-    prop_interrupted/0
+    prop_interrupted/0,
+    prop_chained/0
 ]).
 
 -include_lib("proper/include/proper.hrl").
@@ -111,6 +112,43 @@ prop_interrupted() ->
             ?assertEqual(
                 lists:sublist(lists:map(F, List), TakeN),
                 iterator:to_list(FirstNIter)
+            ),
+            ?assertEqual([], iterator_pmap:flush()),
+            assert_links(Links0),
+            true
+        end
+    ).
+
+%% @doc Make sure several pmap's can be chained.
+prop_chained() ->
+    Gen = {
+        proper_types:list(proper_types:integer()),
+        proper_types:pos_integer(),
+        proper_types:pos_integer()
+    },
+    ?FORALL(
+        {List, C1, C2},
+        Gen,
+        begin
+            F1 = fun(X) -> X + 1 end,
+            F2 = fun(X) -> X * 2 end,
+            ListIter = iterator:from_list(List),
+            Links0 = links(),
+            Pmap1Iter =
+                iterator_pmap:pmap(
+                    F1,
+                    ListIter,
+                    #{concurrency => C1}
+                ),
+            Pmap2Iter =
+                iterator_pmap:pmap(
+                    F2,
+                    Pmap1Iter,
+                    #{concurrency => C2}
+                ),
+            ?assertEqual(
+                lists:map(F2, lists:map(F1, List)),
+                iterator:to_list(Pmap2Iter)
             ),
             ?assertEqual([], iterator_pmap:flush()),
             assert_links(Links0),
